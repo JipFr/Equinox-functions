64a3951a2bbeb975154b432a0fff3ed76c165042
Jip Fr <jipfrijlink@gmail.com>
2021-06-05 15:04:00 +0200

    feat(banner): add anilist banner search

--- functions/banner.js
+++ functions/banner.js
@@ -1,7 +1,26 @@
+const fetch = require("node-fetch");
+
 exports.handler = async (evt) => {
-	const query = event.queryStringParameters.query || null
+	const query = evt.queryStringParameters.query || null;
+
+	const aniListData = await fetch("https://graphql.anilist.co", {
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({
+			query,
+			variables: {
+				search: query,
+				type: "MANGA",
+			},
+		}),
+		method: "POST",
+	}).then((d) => d.json());
+
+	const banner = aniListData?.data?.Media?.bannerImage ?? null;
+
 	return {
 		statusCode: 200,
-		body: `Your query: ${query}`
-	}
-}
+		body: `Your query: ${banner}`,
+	};
+};
